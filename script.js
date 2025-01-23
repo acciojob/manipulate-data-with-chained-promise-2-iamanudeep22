f6515d628dbda5684c7890e290d7a950ee160db6
Anudeep KN <116871481+iamanudeep22@users.noreply.github.com>
2025-01-24 00:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4]
+let resArr  = []
+
+let output = document.getElementById("output")
+
+new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(arr)
+	}, 3000)
+}).then((res) => {
+	resArr = res.filter(val => val%2===0)
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+            console.log(resArr)
+            output.innerText = resArr
+			resolve(resArr)
+		}, 1000)
+	})
+}).then((res) => {
+    resArr = res.map(val => val*2)
+    setTimeout(()=>{
+        output.innerText = resArr
+        console.log(resArr)
+    },2000)
+})
